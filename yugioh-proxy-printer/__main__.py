6fb40ac3fc1773715b591d2e8a54911d8414b967
Christopher Wells <cwellsny@nycap.rr.com>
2021-02-09 23:56:14 -0800

    Grab card image link

--- yugioh-proxy-printer/__main__.py
+++ yugioh-proxy-printer/__main__.py
@@ -2,9 +2,15 @@ from typing import List
 
 import sys
 
+import ygoprodeck
+
 
 def main(args: List[str]) -> None:
     print(args)
+
+    ygo = ygoprodeck.YGOPro()
+
+    print(ygo.get_cards(name="Dark Magician")["data"][0]["card_images"][0]["image_url"])
 
 
 if __name__ == "__main__":
